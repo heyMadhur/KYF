32c5cf5d71f4ce92ab53a58b628006d1388181fb
heyMadhur <madhurgupta12112002@gmail.com>
2024-06-15 15:55:53 +0530

    HomePage Design Done

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,12 +1,37 @@
 import { Stack, SplashScreen } from "expo-router";
 import { useEffect } from "react";
+import { useFonts } from 'expo-font'
+
+
+
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
+
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+    "Fredoka-Bold": require("../assets/fonts/Fredoka-Bold.ttf"),
+    "Fredoka-Light": require("../assets/fonts/Fredoka-Light.ttf"),
+    "Fredoka-Medium": require("../assets/fonts/Fredoka-Medium.ttf"),
+    "Fredoka-Regular": require("../assets/fonts/Fredoka-Regular.ttf"),
+    "Fredoka-SemiBold": require("../assets/fonts/Fredoka-SemiBold.ttf"),
+  });
+
   useEffect(() => {
-    SplashScreen.preventAutoHideAsync();
-    // Hide the splash screen once the layout is rendered
-    SplashScreen.hideAsync();
+    if (error) throw error
+    if (fontsLoaded) SplashScreen.hideAsync();
   }, []);
+
+
+  if (!fontsLoaded && !error) return null;
 
   return (
     <Stack>
--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,15 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import {images} from '../constants'
+import CustomButton from "../components/CustomButton";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-secondary">
-      <Text>Hello Bhai </Text>
+    <View className="flex-1 items-center justify-center bg-primary border-2 border-red relative">
+      <View className="h-full flex-1 items-center justify-center max-h-[80vh] ">
+        <Image className="absolute" source={images.rectangle} />
+        <Image className="" source={images.logo} />
+      </View>
+      <CustomButton title="START" containerStyles="w-[200px]" textStyles="text-red" />
     </View>
   );
 }
